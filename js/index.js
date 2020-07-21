2159f3d46ff2e352328d6efef7fc7e9245deedf8
Administrator <Administrator@ocoss-iMac.local>
2020-07-21 16:23:08 +0530

    first

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).on('submit','#login_form',function(event) {
+
+    var action = $(this).attr('action');
+    var method = $(this).attr('method');
+    var form = $(this);
+
+
+    
+    event.preventDefault();
+
+        $.ajax({
+            url: action,
+            type: method,
+            data: form.serialize(),
+            dataType: "JSON",
+            success: function (data) {
+                var status = data.status;
+                if (status == false) {
+                    $("#alert").html('<div class="alert alert-danger" role="alert">'+data.msg+'</div>')
+                }else{
+                    $("#alert").html('<div class="alert alert-success" role="alert">'+data.msg+'</div>')
+                }
+                console.log('Submission was successful.');
+                console.log(data);
+            },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            },
+        });
+
+});
---
+++ php/login.php
@@ -0,0 +1,47 @@
+<?php  
+ // Database Connection
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "user_management";
+
+try {
+    
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+// Catch the Post Data
+$Name = $_POST['name'];
+$Password = $_POST['password'];
+
+// Valitate Name And Password
+if ($Name == "") {
+  $message = "Pleace enter your name...!";
+  print_r(json_encode(['status'=>false,'msg'=>$message]));exit;
+}
+if ($Password == "") {
+  $message = "Pleace enter your password...!";
+  print_r(json_encode(['status'=>false,'msg'=>$message]));exit;
+}
+
+// Check the database to login
+$result = $conn->prepare("SELECT count(*) FROM user WHERE name='$Name' AND password='$Email'");
+
+$result->execute(); 
+$number_of_rows = $result->fetchColumn();
+
+// Login conditions
+if ($number_of_rows == 0) {
+  $message = "Loin Fail...!";
+  print_r(json_encode(['status'=>false,'msg'=>$message]));exit;
+}else {
+  $message = "Login Successfull...!";
+  print_r(json_encode(['status'=>true,'msg'=>$message]));exit;
+}
+
+?>
