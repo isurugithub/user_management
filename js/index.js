4e1551866cc2900c824487324629348221534f41
Administrator <Administrator@ocoss-iMac.local>
2020-08-06 12:35:43 +0530

    final

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-
- 
-$(document).ready(function() {
-    $('#example').DataTable( {
-        ajax: "php/data_controller.php",
-      'serverSide': true,
-      'serverMethod': 'get',
-        "ordering":true,
-        "paging": true,
-        "sPaginationType": "full_numbers",
-        "processing": true,
-        "bFilter" : true,
-        "searching": true,
-        "responsive": true,
-        "scrollCollapse": true,
-        columns: [ 
-            { data: "name" },
-            { data: "email" },
-            { data: "mobile" },
-            { data: "password" },
-            { data: "address" },  
-        ],
-    } );
-} );
-
-
-
-
 $(document).on('submit','#login_form',function(event) {
 
     var action = $(this).attr('action');
@@ -34,26 +6,179 @@ $(document).on('submit','#login_form',function(event) {
     
     event.preventDefault();
 
+    $.ajax({
+        url: action,
+        type: method,
+        data: form.serialize(),
+        dataType: "JSON",
+        success: function (data) {
+            var status = data.status;
+            if (status == true) {
+                $("#alert").html('<div class="alert alert-success" role="alert">'+data.msg+'</div>')
+                setTimeout(() => {
+                    location.reload();
+                }, 1000);
+            }else{
+                $("#alert").html('<div class="alert alert-danger" role="alert">'+data.msg+'</div>')
+            }
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+            console.log(data);
+        },
+    });
+
+});
+
+$(document).on('submit','#register_form',function(event) {
+    var action = $(this).attr('action');
+    var method = $(this).attr('method');
+    var form = $(this);
+
+    event.preventDefault();
+
+    $.ajax({
+        url: action,
+        type: method,
+        data: form.serialize(),
+        dataType: "JSON",
+        success: function (data) {
+            var status = data.status;
+            if (status == true) {
+                $("#alert").html('<div class="alert alert-success" role="alert">'+data.msg+'</div>')
+                setTimeout(() => {
+                    window.location.replace("http://localhost:8888/projects/index.php");
+                }, 1000);
+            }else{
+                $("#alert").html('<div class="alert alert-danger" role="alert">'+data.msg+'</div>')
+            }
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+            console.log(data);
+        },
+    });
+
+});
+ 
+$(document).ready(function() {
+    if($('#welcome_form').length>0){
+        var userDataTable = $('#welcome_form').DataTable({
+        'processing': true,
+                'serverSide': true,
+                'serverMethod': 'post',
+                'ajax': {
+                    'url':'php/data_controller.php'
+                },
+                'columns': [ 
+                    { data: "name" },
+                    { data: "email" },
+                    { data: "mobile" },
+                    { data: "password" },
+                    { data: "address" }, 
+                    { data: "action" }
+                ],
+        });
+    }
+    
+
+    // Update record
+    $('#welcome_form').on('click','.updateUser',function(){
+        var id = $(this).data('id');
+        document.getElementById('txt_userid').value = id;
+        // $('#txt_userid').val(id);
+        // AJAX request
         $.ajax({
-            url: action,
-            type: method,
-            data: form.serialize(),
-            dataType: "JSON",
-            success: function (data) {
-                var status = data.status;
-                if (status == true) {
-                    $("#alert").html('<div class="alert alert-success" role="alert">'+data.msg+'</div>')
-                    setTimeout(() => {
-                        location.reload();
-                    }, 1000);
-                }else{
-                    $("#alert").html('<div class="alert alert-danger" role="alert">'+data.msg+'</div>')
-                }
-            },
-            error: function (data) {
-                console.log('An error occurred.');
-                console.log(data);
-            },
+        url: 'php/data_controller.php',
+        type: 'post',
+        data: {request: "update_view", id: id},
+        dataType: 'json',
+        success: function(response){
+            console.log(response);
+            console.log(5);
+            if(response.status == 1){
+
+                $('#name').val(response.data.name);
+                $('#email').val(response.data.email);
+                $('#mobile').val(response.data.mobile);
+                $('#password').val(response.data.password);
+                $('#address').val(response.data.address);
+
+                userDataTable.ajax.reload();
+            }else{
+                alert("Invalid ID.");
+            }
+        }
         });
 
+    });
+
+    // Save user 
+    $('#btn_save').click(function(){
+        
+        var id = document.getElementById("txt_userid").value;
+        var name = $('#name').val().trim();
+        var email = $('#email').val().trim();
+        var mobile = $('#mobile').val().trim();
+        var password = $('#password').val().trim();
+        var address = $('#address').val().trim();  
+
+        console.log(name)
+
+        if(name !='' && email != '' && mobile != '' && password != '' && address != ''){
+
+        // AJAX request
+        $.ajax({
+            url: 'php/data_controller.php',
+            type: 'post',
+            data: {request: "update_db", id: id ,name: name, email: email, mobile: mobile, password: password, address: address},
+            dataType: 'json',
+            success: function(response){
+            if(response.status == 1){
+                alert(response.message);
+
+                // Empty and reset the values
+                $('#name','#email','#mobile','#password','#address').val('');
+
+                // Reload DataTable
+                userDataTable.ajax.reload();
+
+                // Close modal
+                $('#updateModal').modal('toggle');
+            }else{
+                alert(response.message);
+            }
+            }
+        });
+
+    }else{
+        alert('Please fill all fields.');
+    }
+    });
+
+    // Delete record
+    $('#welcome_form').on('click','.deleteUser',function(){
+        var id = $(this).data('id');
+
+        var deleteConfirm = confirm("Are you sure?");
+        if (deleteConfirm == true) {
+        // AJAX request
+        $.ajax({
+            url: 'php/data_controller.php',
+            type: 'post',
+            data: {request: "delete_data", id: id},
+            success: function(response){
+                if(response == 1){
+                alert("Record Deleted.");
+
+                // Reload DataTable
+                userDataTable.ajax.reload();
+                }else{
+                alert("Invalid ID.");
+                }
+            }
+        });
+        } 
+    });
+        
 });
--- php/data_controller.php
+++ php/data_controller.php
@@ -1,81 +1,168 @@
 <?php
 require('database.php');
 
-// ## Read value
-// $draw = $_GET['draw'];
-// $row = $_GET['start'];
-// $rowperpage = $_GET['length']; // Rows display per page
-// $columnIndex = $_GET['order'][0]['column']; // Column index
-// $columnName = $_GET['columns'][$columnIndex]['data']; // Column name
-// $columnSortOrder = $_GET['order'][0]['dir']; // asc or desc
-// $searchValue = $_GET['search']['value']; // Search value
+$request = "load_view";
+if(isset($_POST['request'])){
+   $request = $_POST['request'];
+}
 
-// ## Search 
-// $searchQuery = " ";
-// if($searchValue != ''){
-//    $searchQuery = " and (emp_name like '%".$searchValue."%' or 
-//         email like '%".$searchValue."%' or 
-//         city like'%".$searchValue."%' ) ";
-// }
+if($request == "load_view"){
+  ## Read value
+  $draw = $_POST['draw'];
+  $row = $_POST['start'];
+  $rowperpage = $_POST['length']; // Rows display per page
+  $columnIndex = $_POST['order'][0]['column']; // Column index
+  $columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+  $columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+  $searchValue = $_POST['search']['value']; // Search value
 
-// ## Total number of records without filtering
-// $sel = mysqli_query($con,"select count(*) as allcount from employee");
-// $records = mysqli_fetch_assoc($sel);
-// $totalRecords = $records['allcount'];
 
-// ## Total number of record with filtering
-// $sel = mysqli_query($con,"select count(*) as allcount from employee WHERE 1 ".$searchQuery);
-// $records = mysqli_fetch_assoc($sel);
-// $totalRecordwithFilter = $records['allcount'];
-
-// ## Fetch records
-// $empQuery = "select * from employee WHERE 1 ".$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row.",".$rowperpage;
-// $empRecords = mysqli_query($con, $empQuery);
-// $data = array();
-
-// while ($row = mysqli_fetch_assoc($empRecords)) {
-//    $data[] = array( 
-//       "emp_name"=>$row['emp_name'],
-//       "email"=>$row['email'],
-//       "gender"=>$row['gender'],
-//       "salary"=>$row['salary'],
-//       "city"=>$row['city']
-//    );
-// }
-
-// ## Response
-// $response = array(
-//   "draw" => intval($draw),
-//   "iTotalRecords" => $totalRecords,
-//   "iTotalDisplayRecords" => $totalRecordwithFilter,
-//   "aaData" => $data
-// );
-
-// echo json_encode($response);
-
-$result = $conn->prepare("SELECT * FROM user");
-$result->execute([$bar]);
-$row = $result->fetchAll(\PDO::FETCH_ASSOC);
-
-$list = [];
-  
-  foreach ($row as $client) {  
-    $list[] = array(
-      'name' => $client['name'],
-      'email' => $client['email'],
-      'mobile' => $client['mobile'],
-      'password' => $client['password'],
-      'address' => $client['address']
+  $searchArray = array();
+  ## Search 
+  $searchQuery = " ";
+  if($searchValue != ''){
+    $searchQuery = " AND (name LIKE :name OR 
+          email LIKE :email OR 
+          mobile LIKE :mobile OR 
+          address LIKE :address ) ";
+    $searchArray = array( 
+          'name'=>"%$searchValue%", 
+          'email'=>"%$searchValue%",
+          'mobile'=>"%$searchValue%",
+          'address'=>"%$searchValue%"
     );
   }
 
-  $data = [];
-  $data['draw'] = $_GET['draw'];
-  $data['recordsTotal'] = count($list);
-  $data['recordsFiltered'] = count($list);
-  $rowperpage = $_GET['length'];
-  $data['data'] = $list;
+  ## Total number of records without filtering
+  $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM user ");
+  $stmt->execute();
+  $records = $stmt->fetch();
+  $totalRecords = $records['allcount'];
 
-  echo json_encode($data);exit;
+  ## Total number of records with filtering
+  $stmt = $conn->prepare("SELECT COUNT(*) AS allcount FROM user WHERE 1 ".$searchQuery);
+  $stmt->execute($searchArray);
+  $records = $stmt->fetch();
+  $totalRecordwithFilter = $records['allcount'];
+
+  ## Fetch records
+  $stmt = $conn->prepare("SELECT * FROM user WHERE 1 ".$searchQuery." ORDER BY ".$columnName." ".$columnSortOrder." LIMIT :limit,:offset");
+
+  // Bind values
+  foreach($searchArray as $key=>$search){
+    $stmt->bindValue(':'.$key, $search,PDO::PARAM_STR);
+  }
+
+  $stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
+  $stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
+  $stmt->execute();
+  $empRecords = $stmt->fetchAll();
+  $conn = null;
+  $data = array();
+
+  foreach($empRecords as $row){
+    // Update Button
+    $updateButton = "<button class='btn btn-sm btn-info updateUser' data-id='".$row['id']."' data-toggle='modal' data-target='#updateModal' >Update</button>";
+
+    // Delete Button
+    $deleteButton = "<button class='btn btn-sm btn-danger deleteUser' data-id='".$row['id']."'>Delete</button>";
+
+    $action = $updateButton." ".$deleteButton;
+
+    $data[] = array(
+        "name"=>$row['name'],
+        "email"=>$row['email'],
+        "mobile"=>$row['mobile'],
+        "password"=>$row['password'],
+        "address"=>$row['address'],
+        "action" => $action
+    );
+
+  }
+  // DataTable data
+  if($request == "load_view"){
+    ## Response
+    $response = array(
+      "draw" => intval($draw),
+      "iTotalRecords" => $totalRecords,
+      "iTotalDisplayRecords" => $totalRecordwithFilter,
+      "aaData" => $data
+    );
+    echo json_encode($response);exit;
+  }
+}
+
+if($request == "update_view"){
+  $id = $_POST['id'];
+  if ($conn != null) {
+    $result = $conn->prepare("SELECT * FROM user WHERE id='$id'");
+    $result->execute();
+    $conn = null;
+    $response = array();
+    $row = $result->fetchAll(\PDO::FETCH_ASSOC);
+    if ($row != null) {
+      $response = array(                 
+        "name" => $row[0]['name'],
+        "email" => $row[0]['email'],
+        "mobile" => $row[0]['mobile'],
+        "password" => $row[0]['password'],
+        "address" => $row[0]['address']
+      );
+     echo json_encode( array("status" => 1,"data" => $response));exit;
+    }else{
+      echo json_encode( array("status" => 0) );
+      exit;
+    }
+  }
+}
+
+// Update user
+if($request == "update_db"){
+  $id = 0;
+  $id = trim($_POST['id']);// id pass wenne na
+  // Check id
+  if($id != null){
+
+     $name = trim($_POST['name']);
+     $email = trim($_POST['email']);
+     $mobile = trim($_POST['mobile']);
+     $password = trim($_POST['password']);
+     $address = trim($_POST['address']);
+
+     if( $name != '' && $email != '' && $mobile != '' && $password != '' && $address != '' ){
+
+      $result = $conn->prepare("UPDATE user SET name='$name',email='$email',mobile='$mobile',password='$password',address='$address' WHERE id='$id'");
+      $result->execute();
+      echo json_encode( array("status" => 1,"message" => "Record updated.") );
+      exit;
+
+     }else{
+
+      echo json_encode( array("status" => 0,"message" => "Please fill all fields.") );
+      exit;
+
+     }
+
+  }else{
+     echo json_encode( array("status" => 0,"message" => "Invalid ID.") );
+     exit;
+  }
+}
+
+// Delete User
+if($request == "delete_data"){
+  $id = 0;
+  $id = $_POST['id'];
+  if($id > 0){ 
+    $result = $conn->prepare("DELETE FROM user WHERE id='$id'");
+    $result->execute();
+     echo 1;
+     exit;
+  }else{
+     echo 0;
+     exit;
+  }
+}
 
 ?>
+
